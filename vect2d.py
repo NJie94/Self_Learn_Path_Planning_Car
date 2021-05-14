fb6d799372813008f05290920cb519d5503944ad
nicholashi <kaijiehi@gmail.com>
2021-05-14 16:23:22 +0800

    2d plane vector class

---
+++ vect2d.py
@@ -0,0 +1,16 @@
+#Create Vector 2d plane class
+import sys
+
+class vect2d:
+    def __init__(self, x=-1 , y=-1, angle=0):
+        self.x = x
+        self.y = y
+        self.angle = angle
+    
+    def co(self,x,y):
+        self.x = x
+        self.y = y
+    
+    def getCo(self):
+        return (self.x,self.y)
+        
